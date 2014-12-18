14443a35c807da82f9cf3f5f64478fef8836221e
Alfonso Gober <alfonso@wot.io>
2014-12-18 18:34:01 -0500

    BIP-135 Pod refactor is (hopefully) done

--- list.js
+++ list.js
@@ -43,35 +43,29 @@ List.prototype.setup = function(channel, accountInfo, next) {
         modelName = this.$resource.getDataSourceName('track_list');
 
     (function(channel, accountInfo, next) {
-        $resource.file.get(channel, function(err, file, readStream) {
-            if (err) {
-                log(err, channel, 'error');
-                next(err, 'channel', channel);
+        var fileName = self.pod.getDataDir(channel, 'request') + channel.id + ".txt", 
 
-            } else {
-                var listStruct = {
-                    owner_id: channel.owner_id,
-                    channel_id: channel.id,
-                    last_update: $resource.helper.nowUTCSeconds(),
-                    last_build: $resource.helper.nowUTCSeconds()
+        var listStruct = {
+            owner_id: channel.owner_id,
+            channel_id: channel.id,
+            last_update: $resource.helper.nowUTCSeconds(),
+            last_build: $resource.helper.nowUTCSeconds()
+        }
+
+        model = dao.modelFactory(modelName, listStruct, accountInfo);
+
+        dao.create(model, function(err, result) {
+            // touch the  source file
+            $resource.file.save(fileName, new Buffer(""), {write: true}, function(err, file) {
+                if (err) {
+                    log(err, channel, 'error');
+                } else {
+                    log('created container ' + file.localpath, channel);
                 }
 
-                model = dao.modelFactory(modelName, listStruct, accountInfo);
-
-                dao.create(model, function(err, result) {
-                    // touch the  source file
-                    fs.writeFile(file.localpath, "", function(err) {
-                        if (err) {
-                            log(err, channel, 'error');
-                        } else {
-                            log('created container ' + file.localpath, channel);
-                        }
-
-                        next(err, 'channel', channel); // ok
-                    });
-                }, accountInfo);
-            }
-        });
+                next(err, 'channel', channel); // ok
+            });
+        }, accountInfo);
     })(channel, accountInfo, next);
 }
 
@@ -82,19 +76,17 @@ List.prototype.teardown = function(channel, accountInfo, next) {
         log = $resource.log;
 
     // drop list file
-    $resource.file.get(channel, function(err, file, readStream) {
-        if (!err) {
-            fs.unlink(file.localpath);
-            dao.removeFilter(
-                $resource.getDataSourceName('track_list'), {
-                    owner_id: channel.owner_id,
-                    channel_id: channel.id
-                },
-                next
-            );
-        } else {
-            next(err, 'channel', self);
-        }
+    var fileName = self.pod.getDataDir(channel, 'request') + channel.id + ".txt";
+
+    $resource.file.remove(fileName, function(err) {
+        if (err) next(err); 
+        dao.removeFilter(
+            $resource.getDataSourceName('track_list'), {
+                owner_id: channel.owner_id,
+                channel_id: channel.id
+            },
+            next
+        );
     });
 }
 
@@ -107,25 +99,26 @@ List.prototype.rpc = function(method, sysImports, options, channel, req, res) {
         modelName = $resource.getDataSourceName('track_list');
 
     if ('get' === method) {
-        $resource.file.get(channel, function(err, file, fStream) {
-            res.contentType(self.getSchema().renderers.get.contentType);
+        var fileName = self.pod.getDataDir(channel, 'request') + channel.id + ".txt",
+        res.contentType(self.getSchema().renderers.get.contentType);
 
-            if (channel.config.header) {
-                var stream = new Stream();
-                stream.on('data', function(data) {
-                  res.write(data) // change process.stdout to ya-csv
-                });
+        if (channel.config.header) {
+            var stream = new Stream();
+            stream.on('data', function(data) {
+              res.write(data) // change process.stdout to ya-csv
+            });
 
-                stream.emit('data', channel.config.header + '\n');
-            }
+            stream.emit('data', channel.config.header + '\n');
+        }
 
-            try {
+        try {
+            $resource.file.get(fileName, function(err, file, fStream){
                 fStream.pipe(res);
-            } catch (e) {
-                log(e.message, channel, 'error');
-                res.send(500);
-            }
-        });
+            });        
+        } catch (e) {
+            log(e.message, channel, 'error');
+            res.send(500);
+        }
     } else {
         res.send(404);
     }
@@ -143,11 +136,11 @@ List.prototype.invoke = function(imports, channel, sysImports, contentParts, nex
 
     if (imports.line_item) {
         (function(imports, channel, sysImports, next) {
-            $resource.file.get(channel, function(err, file, readStream) {
-                var config = channel.config,
-                    mode = config.write_mode === 'append' ? 'appendFile' : 'writeFile';
+            var fileName = self.pod.getDataDir(channel, 'request') + channel.id + ".txt",
+            config = channel.config,
+            mode = config.write_mode === 'append' ? 'appendFile' : 'writeFile';
 
-                $resource.file.save(fileName, imports.line_item + "\n", { append: config.write_mode }, function(err, struct1) {
+                $resource.file.save(fileName, new Buffer(imports.line_item + "\n"), { append: (mode === 'appendFile'), write: (mode === 'writeFile') }, function(err, struct1) {
                     if (err) {
                         log(err, channel, 'error');
                     } else {
